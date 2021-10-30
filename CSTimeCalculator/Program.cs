656ed9fe82e75b3430cd85d2cdd30071b3ea36bd
nsgwick <nickgoespop@gmail.com>
2021-10-30 10:05:30 +0100

    Initial commit of (simple) program for college

---
+++ CSTimeCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSTimeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many hours have you worked for?");
+            String amount = Console.ReadLine();
+            int hours = Int32.Parse(amount);
+            int minutes = hours * 60;
+            int seconds = minutes * 60;
+            Console.WriteLine($"You have been working for {minutes} minutes and {seconds} seconds. ");
+        }
+    }
+}
